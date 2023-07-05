87eef845ced7eccee6973f096215a2f5a567152e
gemma.garrigosa <gemma.garrigosa@gmail.com>
2023-07-05 12:15:23 +0200

    afegits models

---
+++ webapi/Models/Oficines.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Models
+{
+    public class Oficines
+    {
+        public int OfficeID { get; set; }
+        public string? NomOficina { get; set; }
+        public int CityID { get; set; }
+
+        [ForeignKey ("CityID")]
+
+        public Ciutats ciutat { get; set; }
+    }
+}
---
+++ webapi/Models/Pais.cs
@@ -0,0 +1,9 @@
+﻿namespace webapi.Models
+{
+    public class Pais   
+    {
+        public int CountryID { get; set; }
+        public string? NomPais { get; set; }
+
+    }
+}
