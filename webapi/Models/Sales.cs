f53d236a30aef54ce9d226f9110d1646236614f2
gemma.garrigosa <gemma.garrigosa@gmail.com>
2023-07-06 09:52:10 +0200

    afegits models de Sales i Reserves

---
+++ webapi/Models/Sales.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Models
+{
+    public class Sales
+    {
+        [Key]
+        public int MeetingRoomID { get; set; }  
+        public string? NomSala { get; set; }
+
+        public int OfficeID { get; set; }
+
+        [ForeignKey("OfficeID")]
+        public Oficines oficina { get; set; }
+    }
+}
