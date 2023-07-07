66d4b57dd91f40f70e13e782b114dd29efc509b6
gemma.garrigosa <gemma.garrigosa@gmail.com>
2023-07-07 10:50:42 +0200

    arreclades coses

--- angularapp/src/app/login/login.component.spec.ts
+++ angularapp/src/app/login/login.component.spec.ts
@@ -1,21 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
-import { LoginComponent } from './login.component';
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.css']
+})
+export class LoginComponent {
+  Username: string | undefined;
+  Password: string | undefined;
+  RememberMe: boolean | undefined;
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  constructor(private http: HttpClient, private router: Router) { }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    });
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  login() {
+    const Username = encodeURIComponent(this.Username || '');
+    const Password = encodeURIComponent(this.Password || '');
+    const RememberMe = this.RememberMe || false;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    const url = `https://localhost:7240/Auth/login`;
+    const body = {
+      Username: Username,
+      Password: Password,
+      RememberMe: RememberMe
+    };
+
+    this.http.post(url, body).subscribe(
+      response => {
+        console.log('Login exitoso:', response);
+        this.router.navigate(['/']); // Redirige a la página principal (AppComponent)
+      },
+      error => {
+        console.log('Error en el inicio de sesión:', error);
+      }
+    );
+  }
+}
