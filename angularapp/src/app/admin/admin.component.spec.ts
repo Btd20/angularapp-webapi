3642a3f3829a548cd523747c029e45380e12e2b7
yeray.lorenzo <yeray.lorenzo@inetum.com>
2023-07-12 12:53:16 +0200

    Component d'edició d'usuaris per admins

--- angularapp/src/app/admin/admin.component.spec.ts
+++ angularapp/src/app/admin/admin.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminComponent } from './admin.component';
 
 describe('AdminComponent', () => {
--- angularapp/src/app/admin/admin.component.ts
+++ angularapp/src/app/admin/admin.component.ts
@@ -1,28 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import jwt_decode from 'jwt-decode';
+import { ApiService } from '../api.service';
 
 interface DecodedToken {
   email: string;
   "http://schemas.microsoft.com/ws/2008/06/identity/claims/role": string;
-  // otras propiedades que tengas en el token
+  // altres propietats del token
 }
 
 @Component({
   selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  templateUrl: 'admin.component.html',
+  styleUrls: ['admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  usuaris: any[] = [];
   isAdmin?: boolean;
 
-  constructor() {
+  constructor(private apiService: ApiService) {
     const token = localStorage.getItem('token');
     const decodedToken = token ? jwt_decode(token) as DecodedToken : null;
     this.isAdmin = decodedToken?.["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] === 'Administrador';
 
-    // Guardar el valor del booleano en localStorage
     if (this.isAdmin !== undefined) {
       localStorage.setItem('isAdmin', this.isAdmin.toString());
     }
+  }
+
+  ngOnInit(): void {
+    this.getUsuarisFromApi();
+  }
+
+  getUsuarisFromApi(): void {
+    this.apiService.getUsuaris().subscribe(
+      response => {
+        this.usuaris = response;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  editarUsuario(usuario: any): void {
+    usuario.editando = true;
+  }
+
+  guardarCanvis(usuario: any): void {
+    console.log('Guardar cambios del usuario:', usuario);
+
+    usuario.editando = false;
+  }
+
+  cancelarEdicion(usuario: any): void {
+    usuario.editando = false;
   }
 }
