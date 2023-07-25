7a80d0792fdd0f4da6bad16edd4e5ccd9fc43394
yeray.lorenzo <yeray.lorenzo@inetum.com>
2023-07-25 13:54:41 +0200

    Pujar imatges ja funciona desde el Canviar dades

--- angularapp/src/app/ubicacio/ubicacio.component.ts
+++ angularapp/src/app/ubicacio/ubicacio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-ubicacio',
@@ -7,12 +8,14 @@ import { ApiService } from '../api.service';
   styleUrls: ['./ubicacio.component.css']
 })
 export class UbicacioComponent {
+  fileToUpload: File | null = null;
+
   paisos: any[] = [];
   oficines: any[] = [];
   selectedCountry: string | undefined;
   selectedOffice: string | undefined;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getPaisosFromApi();
@@ -53,5 +56,36 @@ export class UbicacioComponent {
       const username = sessionStorage.getItem('username') ?? '';
       this.apiService.guardarOficina(username, this.selectedOffice);
     }
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.fileToUpload = file;
+    }
+  }
+
+  uploadImage() {
+    if (!this.fileToUpload) {
+      alert("Selecciona una imatge abans de pujarla.");
+      return;
+    }
+
+    const username = sessionStorage.getItem('username'); // Reemplaza "nombreDeUsuario" por el nombre de usuario actual del usuario.
+
+    const formData = new FormData();
+    formData.append("file", this.fileToUpload);
+
+    this.http.post(`https://localhost:7240/api/ApplicationUsers/${username}/UploadProfileImage`, formData)
+      .subscribe(
+        () => {
+          alert("Imatge pujada amb exit.");
+          this.fileToUpload = null;
+        },
+        (error) => {
+          console.error(error);
+          alert("Error al pujar l'imatge.");
+        }
+      );
   }
 }
