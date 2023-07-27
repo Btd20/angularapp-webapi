2604f3c234e869402517b709b2038040a56e4ee8
LaiaRodriguez22 <lrodrigu22@jaumebalmes.net>
2023-07-27 09:46:04 +0200

    Les sales ja es mostren a la reserva. Però s'hauria de fer amb el getSalesFromOficina i no pas el getAllSales

--- angularapp/src/app/fer-reserva/fer-reserva.component.ts
+++ angularapp/src/app/fer-reserva/fer-reserva.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-ferreserva',
@@ -11,20 +12,35 @@ export class FerReservaComponent implements OnInit {
   pais: string | null = sessionStorage.getItem('pais');
   oficina: string | null = sessionStorage.getItem('oficina');
 
+  sales: any[] = [];
+  selectedSala: string | undefined;
+
   paisReserva: string = '';
   ciutatReserva: string = '';
   oficinaReserva: string = '';
   salaReserva: string = '';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.paisReserva = params['pais'];
       this.ciutatReserva = params['ciutat'];
       this.oficinaReserva = params['oficina'];
+      this.getAllSalesFromApi();
       //this.salaReserva = params['sales'];
       //alert(`${this.paisReserva}    ${this.ciutatReserva}   ${this.oficinaReserva}`);
     })
+  }
+
+  getAllSalesFromApi(): void {
+    this.apiService.getAllSales().subscribe(
+      response => {
+        this.sales = response;
+      },
+      error => {
+        console.error(error);
+      }
+    );
   }
 }
