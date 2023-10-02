7ba8a5ce6c9aada285eaeb9d2745ecc3868d6e88
yeray.lorenzo <bergahd@gmail.com>
2023-10-02 10:41:25 +0200

    Password on secret.json

--- webapi/Services/EmailService.cs
+++ webapi/Services/EmailService.cs
@@ -3,16 +3,16 @@ using System.Net.Mail;
 
 public class EmailService
 {
+    private readonly IConfiguration _configuration;
+
+    public EmailService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public void SendEmail(string to, string subject, string body)
     {
-        var emailConfig = new EmailConfiguration
-        {
-            From = "acmerooms@gmail.com",
-            SmtpServer = "smtp.gmail.com",
-            Port = 587,
-            UserName = "acmerooms@gmail.com",
-            Password = "dtid mrgz liap dbjr"
-        };
+        var emailConfig = _configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+        emailConfig.Password = _configuration["EMailKey"];
 
         var smtpClient = new SmtpClient(emailConfig.SmtpServer)
         {
